843d4b8f5c963ce984e66d4da61841c74c54bab6
= <=>
2024-03-16 20:20:50 +0100

    adding more keymaps

--- lua/moises/remap.lua
+++ lua/moises/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("i", "<leader>b", "{}")
-vim.keymap.set("i", "<leader>p", "()")
-vim.keymap.set("n", "<C-b>", "<C-6><CR>")
+vim.keymap.set("i", "<C-b>", "{}<LEFT><CR><CR><kUp><Tab>")
+vim.keymap.set("n", "<C-a>", "<C-6><CR>")
+vim.keymap.set("i", "\"", "\"\"<LEFT>") 
+vim.keymap.set("n", "<leader>pt", "<Cmd>wa<CR>")
