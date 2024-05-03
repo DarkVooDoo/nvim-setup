8affaee00f796174fec0d068ad25dfa2ab6f438f
= <=>
2024-05-03 02:09:18 +0200

    updating telescop and remaps

--- lua/moises/remap.lua
+++ lua/moises/remap.lua
@@ -1,9 +1,15 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "<C-b>", "{}<LEFT><CR><CR><kUp><Tab>")
-vim.keymap.set("n", "<C-a>", "<C-6><CR>")
 vim.keymap.set("i", "\"", "\"\"<LEFT>") 
 vim.keymap.set("i", "(", "()<LEFT>")
 vim.keymap.set("i", "{", "{}<LEFT>")
 vim.keymap.set("i", "[", "[]<LEFT>")
 vim.keymap.set("n", "<leader>pt", "<Cmd>wa<CR>")
+
+
+vim.keymap.set("n", "<leader>ib", "=%")
+vim.keymap.set("n", "<leader>jm", "`a")
+vim.keymap.set("n", "<leader>pe", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "<leader>ne", vim.diagnostic.goto_next)
+
